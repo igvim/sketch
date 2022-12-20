5b93c5f3e7984e23037dcf71f3337f5b431011c4
Isaiah <116988172+igvim@users.noreply.github.com>
2022-12-19 23:27:09 -0500

    Added 16x16 grid with borders

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ create elements
 append them
 make elements square?
 */
+
+container = document.querySelector('.container');
+
+for (i = 0; i < 16**2; i++) {
+    gridItem = document.createElement('div');
+    gridItem.classList.add('item');
+    container.appendChild(gridItem);
+}
